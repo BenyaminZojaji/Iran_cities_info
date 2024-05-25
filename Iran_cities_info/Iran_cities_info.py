d987d7c383fef45e3c56e0a8c85e83916bcd1e95
BenyaminZojaji <benyamin.zojaji@gmail.com>
2024-05-25 12:49:59 +0330

    fix path in prev patch, try to fix check.py

--- Iran_cities_info/Iran_cities_info.py
+++ Iran_cities_info/Iran_cities_info.py
@@ -1,7 +1,7 @@
 import os
 import json
 import pathlib
-# import check
+import check
 
 
 def get_completeData(lang: str='en'): 
@@ -32,7 +32,7 @@ def get_coordinates(_city: str='', lang: str='en'):
 
     lang = en | fa
     '''
-    if not empty_argument(_city):
+    if not check.empty_argument(_city):
         return {'error': 'empty argument'}
     
     data = get_completeData(lang=lang)
@@ -48,7 +48,7 @@ def search_province(_province: str='', lang: str='en'):
 
     lang = en | fa
     '''
-    if not empty_argument(_province):
+    if not check.empty_argument(_province):
         return {'error': 'empty argument'}
     
     data = get_completeData(lang=lang)
@@ -63,7 +63,7 @@ def search_center(_center: str='', lang: str='en'):
 
     lang = en | fa
     '''
-    if not empty_argument(_center):
+    if not check.empty_argument(_center):
         return {'error': 'empty argument'}
     
     data = get_completeData(lang=lang)
@@ -72,8 +72,8 @@ def search_center(_center: str='', lang: str='en'):
             return state
 
 
-def empty_argument(argument):
-    return False if argument=='' else True
+# def empty_argument(argument):
+#     return False if argument=='' else True
 
 ## ---------- This part is only for my classroom assignment, It will be remove in future ----------
 import wikipedia 
@@ -87,4 +87,4 @@ def joke():
 
 ## ---------- ---------- ---------- ---------- ---------- ---------- ---------- ---------- ----------
 
-print(get_completeData())
+print(get_coordinates('Shiraz'))
